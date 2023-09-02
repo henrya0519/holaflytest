10684860e1a70853d96de26b1292ce2c066d077e
henry.arevalo <henry.arevalo@external.cardif.com.co>
2023-09-02 13:34:16 -0500

    refactor(abstractPeople) remove init from abstractPeople

---
+++ src/app/People/abstractPeople copy.js
@@ -0,0 +1,54 @@
+class WookieePeople {
+
+    constructor(id,name,mass,height,homeworldName,homeworlId) {
+        if (this.constructor == AbstractPeople) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+        this.id = id
+        this.name = name
+        this.mass = mass
+        this.height = height
+        this.homeworldName = homeworldName
+        this.homeworlId = homeworlId
+    }
+
+    getId() {
+       return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getMass() {
+        return this.mass;
+    }
+
+    getHeight() {
+        return this.height;
+    }
+
+    getHomeworldName() {
+        return this.homeworldName;
+    }
+
+    getHomeworlId() {
+        return this.homeworlId;
+    }
+
+    getWeightOnPlanet(planetId){
+        throw new Error('To be implemented');
+    }
+
+    getPerson(){
+        return {
+            name: this.getName(),
+            mass: this.getMass(),
+            height: this.getMass(),
+            homeworldName: this.getHomeworldName(),
+            homeworldId: this.getHomeworlId(),
+        }
+    }
+}
+
+module.exports = WookieePeople
